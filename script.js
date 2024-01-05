a48357f2974987d9482dceb93bcef1c4a9154831
Mishra vibhakar <mishra.vibhakar7262@gmail.com>
2024-01-06 01:58:37 +0530

    done

---
+++ script.js
@@ -0,0 +1,11 @@
+const div = document.getElementById("root");
+
+div.style.backgroundColor="yellow";
+div.style.margin="20px";
+div.style.padding="10px";
+div.style.height ="200px";
+div.style.width="300px";
+
+const h1 = div.querySelector("h1")
+h1.style.fontSize="18px";
+h1.style.fontWeight="bold";
